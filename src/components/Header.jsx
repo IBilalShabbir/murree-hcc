d17c75696bce224b02f0ade5f307f4d0b6a603db
IBilalShabiir <mianbilal.4288@gmail.com>
2023-01-17 16:00:44 +0500

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ export default function Header() {
 
     {
       label: "Pricing",
-      to: "/createownyourmail",
+      to: "/about",
     },
   ];
   return (
--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -1,22 +1,5 @@
-import CreateOwnYourMail from "../components/CreateOwnYourMail";
-import HomePageTrading from "../components/HomePageTrading";
-import HomepageBanner from "../components/HomeBanner";
-import MostAdvancedCrypto from "../components/MostAdvancedCrypto";
-import { useEffect } from "react";
+import React from "react";
 
-export default function Index() {
-  useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  }, []);
-  return (
-    <>
-      <HomepageBanner />
-      <HomePageTrading />
-      <MostAdvancedCrypto />
-      <CreateOwnYourMail />
-    </>
-  );
+export default function index() {
+  return <div>index</div>;
 }
