304dea929486e0d08390aca6de4fbd09e32f40f4
IBilalShabiir <mianbilal.4288@gmail.com>
2023-01-20 10:18:51 +0500

    footer done

--- src/components/Cottages.jsx
+++ src/components/Cottages.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { PngCardimgone, PngCardimgthree, PngCardimgtwo } from "../assets";
 
 export default function Cottages() {
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,78 @@
+import { Facebook, Instagram, Linkedin, Youtube } from "react-feather";
+import { PngHolidaycountry, PngLogo } from "../assets";
+import { Link } from "../router";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  const navLinks = [
+    {
+      label: "Features",
+      to: "/features",
+    },
+    {
+      label: "Inventory",
+      to: "/inventory",
+    },
+    {
+      label: "Location",
+      to: "/location",
+    },
+  ];
+  return (
+    <div className="footer">
+      <img
+        src={PngHolidaycountry}
+        alt="holidaycountry"
+        className="footer__img"
+      />
+      <div className="footer__container">
+        <img src={PngLogo} alt="logo" className="footer__container__top_img" />
+        <div className="footer__container__heading">Want to know more?</div>
+        <div className="footer__container__text">
+          Dream Home is a gated community with a great location. Located
+          downtown, you’re within walking distance of Parks, and the best
+          shopping, dining and entertainment Getting around is a breeze, with
+          easy access to freeways, buses and trolleys.
+        </div>
+        <div className="footer__container__btn__entry">
+          <button className="footer__container__btn">Get Started</button>
+        </div>
+        <div className="footer__container__btn__entry__hr"></div>
+        <div className="footer__container__bottom">
+          <div className="footer__container__bottom__left">
+            <img
+              src={PngLogo}
+              alt="logo"
+              className="footer__container__bottom__left__img"
+            />
+          </div>
+          <div className="footer__container__bottom__middle">
+            {navLinks.map((link) => {
+              return (
+                <Link
+                  className="footer__container__bottom__middle__link"
+                  to={link.to}
+                >
+                  {link.label}
+                </Link>
+              );
+            })}
+          </div>
+          <div className="footer__container__bottom__right">
+            <a href="#" className="footer__container__bottom__right__entry">
+              <Linkedin />
+            </a>
+            <a href="#" className="footer__container__bottom__right__entry">
+              <Facebook />
+            </a>
+            <a href="#" className="footer__container__bottom__right__entry">
+              <Instagram />
+            </a>
+            <a href="#" className="footer__container__bottom__right__entry">
+              <Youtube />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
