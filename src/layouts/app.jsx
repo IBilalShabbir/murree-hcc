8fae8bb5ac48080749450cdf9114cb08937cbcda
IBilalShabiir <mianbilal.4288@gmail.com>
2023-01-22 18:27:27 +0500

    header name and logo update

--- src/layouts/app.jsx
+++ src/layouts/app.jsx
@@ -4,8 +4,8 @@ import { Head } from "router";
 import { Outlet } from "react-router-dom";
 
 export default function App() {
-  const title = "Vite file router";
-  const description = "Vite file router";
+  const title = "Holiday Country Club";
+  const description = "Holiday Country Club";
   const image = "/vite.svg";
   const url = "https://vitefilerouter.com";
   return (
